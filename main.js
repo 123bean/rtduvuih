4db9c6225ddb7df46abd6e3aa2f9e80797645211
123bean <86067945+123bean@users.noreply.github.com>
2021-06-17 11:30:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function spanelem(){
+document.getElementById("audio1").play();
+window.alert("this is a span element");}
+function divelem(){
+document.getElementById("audio1").play();
+window.alert("this is a div element");}
